be438f551310f55da6e67a6c2d9c8543428f4f47
Davematteer <davematteer@gmail.com>
2024-09-22 20:03:53 +0000

    clean up

--- Storingusers.java
+++ Storingusers.java
@@ -1,7 +1,7 @@
-import Authentication;
 import java.util.Map;
 import java.util.HashMap;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -11,27 +11,53 @@ public class Storingusers{
  //So i think i would use a map 
 
   private File file = new File("users.txt");
-  private Scanner scanner = new Scanner(file);
- 
-  public void filecreation(){
+  private Scanner scanner;
+
+  public void filecreationandreading(){
     try {
+      scanner = new Scanner(file);
+
       if (file.createNewFile()){
         System.out.println("File created: "+ file.getName());   
       }
       else{
         System.out.println("File already exist");
       }
-    } catch (IOException e){
-      System.out.println("An error occurred");
-      e.printStackTrace();
+    } catch (FileNotFoundException e){
+      System.out.println("An error occurred: "+ e.getMessage());
     }
+    catch (IOException e){
+      System.out.println("An error occurred"+ e.getMessage());
+      e.printStackTrace();
+
+    } finally{
+      
+      if (scanner != null){
+        scanner.close();
+      }
+    }
+    while (scanner.hasNextLine()){
+      //TODO: Check if username is present to compare
+      // or we can use it to prevent reusing a username, but we can do this with a hashmap so
+      //there may be no need.
+    }
+  }
+
+  public void writeFile(){
+    try{
+     scanner = new Scanner(file);
+     
+    }
+    catch(IOException e){}
+    finally{scanner.close();}
+
   }
 
   public Map<String,String> database(){
 
     final Map<String , String> users = new HashMap<>();  
     
-    
+    return users; 
   }  
   
 
